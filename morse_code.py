29d6af83aeab64de616bbc7b4d629bf569a05e2f
tawsyfbro <127408370+tawsyfbro@users.noreply.github.com>
2023-03-09 21:53:10 +0600

    Update morse_code.py

--- morse_code.py
+++ morse_code.py
@@ -4,22 +4,6 @@ morse_code_dict = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F
                    'Y': '-.--', 'Z': '--..', '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
                    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.'}
 
-
-# def convert_to_morse_code(text):
-#     morse_code = ''
-#     for char in text.upper():
-#         if char != ' ':
-#             morse_code += morse_code_dict[char] + ' '
-#         else:
-#             morse_code += ' '
-#     return morse_code
-
-
-# # example usage
-# text = 'HELLO WORLD'
-# morse_code = convert_to_morse_code(text)
-# print(morse_code)
-
 user_input = input("Type the text you want to convert into Morse Code: ")
 
 characters = []
